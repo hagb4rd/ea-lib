ff6822097321fb49ac7d203367b5465f79bbad9d
hagb4rd <hagb4rd@gmail.com>
2018-12-10 07:13:56 +0100

    repl fixed

--- .bin/repl.js
+++ .bin/repl.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 #!/usr/bin/env node
-=======
->>>>>>> db852ed98670fc9ea535acb3ca29a71ddbe7b93c
 var repl=require('../repl')(process.argv.slice(2).join(' ')||'repl.commands.help.action.call(repl)');
